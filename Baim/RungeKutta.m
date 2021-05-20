0848ece6296e70266aa4e0987310a3ed7a5a4a43
Duc Nguyen <buuduc1999@gmail.com>
2021-05-21 03:03:39 +0700

    dang lam bai gauss seidel

--- Baim/RungeKutta.m
+++ Baim/RungeKutta.m
@@ -1,4 +1,23 @@
 clc
 clear all
-dy= @(x,y) 2*x +x*sin(x+2*y)
-X=
+
+%% input
+dy= @(x,y) 4*x+x*sin(x+2*y)
+x0=1;
+y0=3.2;
+h=0.2
+xEnd=2.2
+
+%% hanlde
+X= [x0];
+Y=[y0];
+X=[x0:h:xEnd]';
+
+for k=2:length(X)
+    K1(k,1)=h*dy(X(k-1),Y(k-1));
+    K2(k,1)= h*dy(X(k-1)+h/2,Y(k-1)+K1(k)/2);
+    K3(k,1)=h*dy(X(k-1)+h/2,Y(k-1)+K2(k)/2);
+    K4(k,1)=h*dy(X(k-1)+h,Y(k-1)+K3(k));
+    Y(k,1)=Y(k-1)+(K1(k)+2*K2(k)+2*K3(k)+K4(k))/6;
+end
+table(X,K1,K2,K3,K4,Y)
--- baik/BaiK.m
+++ baik/BaiK.m
@@ -1,12 +1,12 @@
 clc;
 clear all;
 dy= @(x,y) 4*x+x*sin(x+2*y);
-firstPoint=1;
+x0=1; %x0 = doe, bat dau
 y0=3.2
 finalPoint=2.2;
 h=0.2
 Y=[y0];
-range = [firstPoint finalPoint];
+range = [x0 finalPoint];
 X=[range(1):h:range(2)]
 for i =1 :length(X) -1 
     Y(i+1)=Y(i)+h*dy(X(i),Y(i))
